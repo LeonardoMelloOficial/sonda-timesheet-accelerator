94a09a3a1bfa2ec6d63288865eb05099709e06e7
Y2K8 <y2k8@br-petrobras.com.br>
2019-07-16 13:42:10 -0300

    Initial version

---
+++ src/main/java/br/com/sondait/timesheet/TimeSheetApplication.java
@@ -0,0 +1,104 @@
+package br.com.sondait.timesheet;
+
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.sondait.timesheet.config.SeleniumHelper;
+
+@SpringBootApplication
+@RestController
+public class TimeSheetApplication {
+
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeSheetApplication.class, args);
+	}
+
+	@PostMapping("/register")
+	public ResponseEntity register(
+			@RequestParam("userId") String userId, 
+			@RequestParam("password") String password, 
+			@RequestParam("action") String action) {
+
+		Map<String, String> retorno = new HashMap<String, String>();
+		SeleniumHelper helper = new SeleniumHelper();
+
+		try {
+
+			if (userId == null || userId.isEmpty()){
+				throw new RuntimeException("invalid userId");
+			}
+			
+			if (password == null || password.isEmpty()) {
+				throw new RuntimeException("invalid password");
+			}
+
+			if (action == null || action.isEmpty()) {
+				throw new RuntimeException("invalid action");
+			} 
+
+			if (!"ponto".equals(action) && !"intervalo".equals(action)){
+				throw new RuntimeException("invalid action");
+			}
+
+			WebDriver driver = helper.getDriver();
+
+			String passwordEncoded = URLEncoder.encode(password);
+
+			StringBuilder url = new StringBuilder();
+			url.append("https://");
+			url.append(userId);
+			url.append(":");
+			url.append(passwordEncoded);
+			url.append("@pontoeletronico.sondait.com.br/Point/Index");
+
+			driver.get(url.toString());
+
+			System.out.println(driver.getPageSource());
+
+			driver.findElement(By.id("createNewPoint")).click();
+
+			if ("ponto".equals(action)){
+				driver.findElement(By.id("pointImage")).click();
+			}
+
+			if ("intervalo".equals(action)) {
+				driver.findElement(By.id("mealImage")).click();
+			}
+
+			driver.findElement(By.id("Save")).click();
+
+			retorno.put("success", "true");
+			// retorno.put("code", driver.getPageSource());
+
+			// System.out.println(driver.getPageSource());
+
+		
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			retorno.put("success", "false");
+			if (e.getMessage() != null) {
+				retorno.put("msg", e.getMessage());
+			}
+			return new ResponseEntity<Map<String, String>>(retorno, HttpStatus.INTERNAL_SERVER_ERROR);
+
+		}
+
+		// helper.navigateTo("www.google.com");
+		return new ResponseEntity<Map<String, String>>(retorno, HttpStatus.OK);
+	}
+}
---
+++ src/main/java/br/com/sondait/timesheet/config/SeleniumHelper.java
@@ -0,0 +1,58 @@
+package br.com.sondait.timesheet.config;
+
+import java.io.File;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SeleniumHelper {
+
+    private WebDriver driver;
+
+    public SeleniumHelper() {
+        // Capabilities capabilities = DesiredCapabilities.firefox();
+        // driver = new FirefoxDriver(capabilities);
+        // this.driver = new
+        // JBrowserDriver(Settings.builder().timezone(Timezone.AMERICA_DENVER).build());
+        // driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200", "--ignore-certificate-errors",
+                "--silent");
+        this.driver = new ChromeDriver(options);
+
+    }
+
+    static {
+        // System.setProperty("webdriver.gecko.driver", findFile("geckodriver.mac"));
+        System.setProperty("webdriver.chrome.driver", findFile("chromedriver.exe"));
+    }
+
+    static private String findFile(String filename) {
+        String paths[] = { "", "bin/", "target/classes" }; 
+                                                           
+        for (String path : paths) {
+            if (new File(path + filename).exists())
+                return path + filename;
+        }
+        return "";
+    }
+
+    public void close() {
+        driver.close();
+    }
+
+    public void navigateTo(String url) {
+        driver.navigate().to(url);
+    }
+
+    public void clickElement(WebElement element) {
+        element.click();
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+}
