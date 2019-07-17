8552940432102c7f816412b68b0d321acce24c41
Y2K8 <y2k8@br-petrobras.com.br>
2019-07-17 16:41:23 -0300

    Swagger doc

--- src/main/java/br/com/sondait/timesheet/TimeSheetApplication.java
+++ src/main/java/br/com/sondait/timesheet/TimeSheetApplication.java
@@ -4,6 +4,8 @@ import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,14 +13,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 import br.com.sondait.timesheet.config.SeleniumHelper;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class TimeSheetApplication {
 
 	
@@ -27,78 +32,14 @@ public class TimeSheetApplication {
 		SpringApplication.run(TimeSheetApplication.class, args);
 	}
 
-	@PostMapping("/register")
-	public ResponseEntity register(
-			@RequestParam("userId") String userId, 
-			@RequestParam("password") String password, 
-			@RequestParam("action") String action) {
-
-		Map<String, String> retorno = new HashMap<String, String>();
-		SeleniumHelper helper = new SeleniumHelper();
-
-		try {
-
-			if (userId == null || userId.isEmpty()){
-				throw new RuntimeException("invalid userId");
-			}
-			
-			if (password == null || password.isEmpty()) {
-				throw new RuntimeException("invalid password");
-			}
-
-			if (action == null || action.isEmpty()) {
-				throw new RuntimeException("invalid action");
-			} 
-
-			if (!"ponto".equals(action) && !"intervalo".equals(action)){
-				throw new RuntimeException("invalid action");
-			}
-
-			WebDriver driver = helper.getDriver();
-
-			String passwordEncoded = URLEncoder.encode(password);
-
-			StringBuilder url = new StringBuilder();
-			url.append("https://");
-			url.append(userId);
-			url.append(":");
-			url.append(passwordEncoded);
-			url.append("@pontoeletronico.sondait.com.br/Point/Index");
-
-			driver.get(url.toString());
-
-			System.out.println(driver.getPageSource());
-
-			driver.findElement(By.id("createNewPoint")).click();
-
-			if ("ponto".equals(action)){
-				driver.findElement(By.id("pointImage")).click();
-			}
-
-			if ("intervalo".equals(action)) {
-				driver.findElement(By.id("mealImage")).click();
-			}
-
-			driver.findElement(By.id("Save")).click();
-
-			retorno.put("success", "true");
-			// retorno.put("code", driver.getPageSource());
-
-			// System.out.println(driver.getPageSource());
-
-		
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			retorno.put("success", "false");
-			if (e.getMessage() != null) {
-				retorno.put("msg", e.getMessage());
-			}
-			return new ResponseEntity<Map<String, String>>(retorno, HttpStatus.INTERNAL_SERVER_ERROR);
-
-		}
-
-		// helper.navigateTo("www.google.com");
-		return new ResponseEntity<Map<String, String>>(retorno, HttpStatus.OK);
+	@GetMapping(value = "/")
+	public RedirectView swaggerJson(HttpServletRequest request){
+		return new RedirectView("http://petstore.swagger.io/?url="+request.getRequestURL()+"swagger.yaml");
 	}
+
+	// @GetMapping(value = "/swagger.yaml")
+	// public String swaggerYaml() {
+	// 	return "swagger.yaml";
+	// }
+
 }
